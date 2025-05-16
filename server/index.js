551c78b8e03e8e12ab63f055d3482b559a993df0
LaKriim <karimrekik82@gmail.com>
2025-05-15 23:08:25 -0400

    server index file

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
